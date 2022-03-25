76379387c6bd3e0273387a6c316a377df864020a
nagima Dubanaeva <98920760+nagimad@users.noreply.github.com>
2022-03-25 00:56:12 +0000

    part A

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Turtle Drat Game
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 1
 
-Grace days remaining: < # >/5
+Grace days remaining: 4/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ part B
 
 ## REFERENCES:
- < List any outside resources used >
+ used the python library
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ n/a
--- main.py
+++ main.py
@@ -31,7 +31,61 @@ import time
 #########################################################
 #                   Your Code Goes Below                #
 #########################################################
+def drawSquare(x,y,width):
+    myPen.penup()
+    myPen.goto(x,y)
+    myPen.pensize(3)
+    myPen.color("#333333")
+    myPen.pendown()
+    for side in range(0,4):
+        myPen.forward(width)
+        myPen.right(90)
+        myPen.pensize(1)
+def drawCircle(x,y,radius):
+    myPen.penup()
+    myPen.goto(x,y-radius)
+    myPen.pensize(2)
+    myPen.color("#333333")
+    myPen.pendown()
+    myPen.circle(radius)
+    myPen.pensize(1)
 
+def drawDot(x,y,color):
+    myPen.penup()
+    myPen.goto(x,y-1)
+    myPen.pendown()
+    myPen.fillcolor(color)
+    myPen.color(color)
+    myPen.begin_fill()
+    myPen.circle(1)
+    myPen.end_fill()
+    radius=180
+
+color = "#000000"
+total = 2500
+totalIn = 0
+
+drawSquare(-radius,radius,2*radius)
+drawCircle(0,0,radius)
+
+for dots in range(0,total):
+    x = random.randint(-radius,radius)
+    y = random.randint(-radius,radius)
+
+    distance = (x**2 + y**2)**0.5
+    if distance<radius:
+        color = "#FF0000"
+        totalIn += 1
+    else:
+        color = "#0000FF"
+
+    drawDot(x,y,color)
+
+myPen.getscreen().update()
+
+
+pi = 4*(totalIn/total)
+print("Pi Estimation:" + str(pi))
 
 
 #########################################################
@@ -51,10 +105,10 @@ def main():
     window = turtle.Screen()
     darty = turtle.Turtle()
     darty.speed(0) # as fast as it will go!
-    setUpDartboard(window, darty)
+setUpDartboard(window, darty)
 
     # Loop for 10 darts to test your code
-    for i in range(10):
+for i in range(10):
         throwDart(darty)
     print("\tPart A Complete...")
     print("=========== Part B ===========")
